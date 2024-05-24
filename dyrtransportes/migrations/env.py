c9cec186d2e51664c964c82fca59b33886ebba54
Rolando Medina Rosner <rolmedro@gmail.com>
2024-05-24 00:13:48 -0400

    feat: added autogenerate to Alembic

--- dyrtransportes/migrations/env.py
+++ dyrtransportes/migrations/env.py
@@ -9,6 +9,8 @@ from pathlib import Path
 
 from alembic import context
 
+from dyrtransportes.models import Base
+
 project_root_path = Path(__file__).parent.absolute().parent
 load_dotenv(f'{project_root_path}/.env')
 
@@ -31,8 +33,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
