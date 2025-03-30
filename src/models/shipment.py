cdcfdcb80edea08e1c2d0da9e1f18ed2504173ce
Rolando Medina Rosner <rolmedro@gmail.com>
2025-03-30 00:00:08 -0300

    fix: shipment unique constraint

--- src/models/shipment.py
+++ src/models/shipment.py
@@ -111,7 +111,6 @@ class Shipment(Base):
             "shipment_date",
             "route_code",
             "product_code",
-            "deleted",
             name="unique_driver_ticket_date",
         ),
     )
